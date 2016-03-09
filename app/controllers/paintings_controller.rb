cbc698e4d28f36ceb4ce460c3a12ae9de277beea
Drew Wasserman <druzi312@gmail.com>
2016-03-09 16:37:04 -0600

    Add/Delete Paintings

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,5 +1,7 @@
 class PaintingsController < ApplicationController
-  	
+	
+	include PaintingsHelper
+
 	def index
 		@paintings = Painting.all
 		#@users = User.paginate(:page => params[:page], :per_page => 5)
@@ -9,5 +11,21 @@ class PaintingsController < ApplicationController
 		@painting = Painting.find(params[:id])
 	end
 
+	def new
+		@painting = Painting.new
+	end
+
+	def create
+ 	  @painting = Painting.new(painting_params)
+	  @painting.save
+	  redirect_to painting_path(@painting)
+
+	end
+
+	def destroy
+		@painting = Painting.find(params[:id])
+	    @painting.destroy
+	    redirect_to painting_path(@painting)		
+	end	
 
 end  
--- app/helpers/paintings_helper.rb
+++ app/helpers/paintings_helper.rb
@@ -1,2 +1,7 @@
 module PaintingsHelper
+
+	def painting_params
+		params.require(:painting).permit(:title, :body)
+	end
+	
 end
