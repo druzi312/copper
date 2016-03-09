125b9d74e5fb2b9e26a5096c48c13d7cc0b93677
Drew Wasserman <druzi312@gmail.com>
2016-03-09 17:20:22 -0600

    Add/Delete Paintings II

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,6 +1,8 @@
 class PaintingsController < ApplicationController
 	
 	include PaintingsHelper
+	#before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+	#before_action :admin_user,     only: :destroy
 
 	def index
 		@paintings = Painting.all
@@ -19,13 +21,25 @@ class PaintingsController < ApplicationController
  	  @painting = Painting.new(painting_params)
 	  @painting.save
 	  redirect_to painting_path(@painting)
-
+	  flash[:success] = "Painting Added"
 	end
 
 	def destroy
 		@painting = Painting.find(params[:id])
 	    @painting.destroy
-	    redirect_to painting_path(@painting)		
+	    redirect_to action: "index"		
 	end	
+
+	def edit
+	  @painting = Painting.find(params[:id])
+	end
+
+	def update
+	  @painting = Painting.find(params[:id])
+	  @painting.update(painting_params)
+	  redirect_to painting_path(@painting)
+	  flash[:success] = "Painting Updated"
+	end
+
 
 end  
