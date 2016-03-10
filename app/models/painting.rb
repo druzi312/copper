0753bc3f30c72706f9ba0599d92efa9a2c16566c
Drew Wasserman <druzi312@gmail.com>
2016-03-10 09:00:45 -0600

    Image Sizes

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,6 +1,6 @@
 class Painting < ActiveRecord::Base
-	
-	has_attached_file :image
+
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>", large: "600x600>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 end
